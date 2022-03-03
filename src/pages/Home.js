0c39c30b81e7f65ef5e1da1e5f33b31def3ea47f
unknown <clamy6@yahoo.fr>
2022-03-03 16:59:35 +0100

    navigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useNavigate } from "react-router-native";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <View>
       <Text>Home</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigate(-1);
+        }}
+      >
+        <Text>Welcome</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,44 @@
-import { StyleSheet, View, Text, SafeAreaView, TextInput } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigate } from "react-router-native";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    if (Login.length > 10) {
+      navigate("/home");
+      console.log("Submit");
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Login</Text>
 
       <View style={styles.subContainer}>
         <TextInput style={styles.input} placeholder="Login" />
+        <TouchableOpacity onPress={() => handleSubmit()}>
+          <Text>Button</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#fff" },
-  subContainer: { flex: 1, alignItems: "center", justifyContent: "center" },
+  container: { backgroundColor: "#fff", marginTop: 100 },
+  subContainer: { alignItems: "center", justifyContent: "center" },
   input: {
     width: 250,
     borderWidth: 1,
     borderRadius: 8,
     padding: 10,
     marginBottom: 5,
+    marginTop: 100,
   },
   title: { fontWeight: "bold", fontSize: 30, textAlign: "center" },
 });
